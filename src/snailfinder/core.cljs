7f9bf63c3136ec85b85628df7c631fcd7e1b2b98
Peter Belko <peter.belko@brightnorth.co.uk>
2015-07-18 20:19:31 +0100

    Snail page

--- src/snailfinder/core.cljs
+++ src/snailfinder/core.cljs
@@ -3,6 +3,7 @@
             [om.dom :as dom :include-macros true]
             [snailfinder.data :refer [app-state]]
             [snailfinder.key-views :refer [snail-key-view]]
+            [snailfinder.snail-view :refer [snail-view]]
             ))
 
 (enable-console-print!)
@@ -15,19 +16,23 @@
   [cursor _]
   (om/component
     (dom/nav #js{:className "navbar navbar-default"}
-             (dom/div #js{:className "container-fluid"}
-                      (dom/div #js{:className "navbar-header"}
-                               (dom/div #js{:className "navbar-brand"
-                                            :onClick   #(do
-                                                         (om/update! cursor [:current :page] :home)
-                                                         (om/update! cursor [:current :question] :c1))} "Snail Finder"))))))
+      (dom/div #js{:className "container-fluid"}
+        (dom/div #js{:className "navbar-header"}
+          (dom/div #js{:className "navbar-brand"
+                       :onClick   #(do
+                                    (om/update! cursor [:current :page] :home)
+                                    (om/update! cursor [:current :question] :c1))} "Snail Finder"))))))
 
 
 (defn home-component
   [cursor _]
   (om/component
     (dom/div nil
-      (dom/a #js {:onClick #(om/update! cursor [:current :page] :snail-key)} "Find a snail"))))
+      (dom/a #js {:onClick #(om/update! cursor [:current :page] :snail-key)} "Find a snail")
+      (dom/br nil)
+      (dom/a #js{:onClick   #(do
+                              (om/update! cursor [:current :page] :snail)
+                              (om/update! cursor [:current :question] :ae2))} "Snail Page"))))
 
 (defn main-component
   [cursor _]
@@ -35,17 +40,17 @@
     (dom/div
       #js{:className "container"}
 
-    ;(om/component
+      ;(om/component
       (dom/div
         #js{:className "container"}
         (dom/div
           #js{:className "row"}
-                 (dom/div #js{:className "col-sm-12"}
-                   (case (get-in cursor [:current :page])
-                   :home (om/build home-component cursor)
-                   :snail-key (om/build snail-key-view cursor)
-                   (om/build home-component cursor))))
-        )
+          (dom/div #js{:className "col-sm-12"}
+            (case (get-in cursor [:current :page])
+              :home (om/build home-component cursor)
+              :snail-key (om/build snail-key-view cursor)
+              :snail (om/build snail-view cursor)
+              (om/build home-component cursor)))))
       ;)
       )))
 
@@ -54,8 +59,8 @@
     (om/component
 
       (dom/div nil
-               (om/build header data)
-               (om/build main-component data)
+        (om/build header data)
+        (om/build main-component data)
         )))
   app-state
   {:target (. js/document (getElementById "app"))})
