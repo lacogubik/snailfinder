2856237b42309ec77c6475dd8bd6fc392e0252e0
Federico Roffi <fedoverseas@gmail.com>
2015-09-26 16:53:18 +0100

    Added mocked snail images

--- src/snailfinder/snails.cljs
+++ src/snailfinder/snails.cljs
@@ -31,11 +31,11 @@
                                        (subsumes (lower-case (:name value)) (lower-case query)))) snails)]
               [:div.mdl-grid
                [:div.mdl-cell.mdl-cell--12-col
-                [:h2 "Snails"]
+                ;[:h2 "Snails"]
                 [:div.mdl-textfield.mdl-js-textfield.mdl-textfield--floating-label {:ref "snails-query"}
                  [:input.mdl-textfield__input {:value     (get-in app [:snails :query])
                                                :on-change (fn [e]
                                                             (om/update! app [:snails :query] (.. e -target -value)))}]
                  [:label.mdl-textfield__label "Search"]]
                 (into [:div] (map (fn [[key snail]]
-                                    [:div (str (:name snail))]) result))]])))))
+                                    [:div [:img {:src (str "https://placeholdit.imgix.net/~text?txtsize=33&w=350&h=350&txt=" (:name snail))}] [:div (str (:name snail))]]) result))]])))))
