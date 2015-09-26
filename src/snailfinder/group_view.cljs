90e80c15918585b1dc5d203dd7169ef2af63cff4
= <sassela@live.com>
2015-09-26 19:15:31 +0100

    Change all Om to Sablono

--- src/snailfinder/group_view.cljs
+++ src/snailfinder/group_view.cljs
@@ -13,22 +13,20 @@
 (defn group-list-component
   [group cursor _]
   (om/component
-    (html
-      [:div
-       [:a {:href "#/"} "back button"]
-       [:h4 "Your snail could be one of these:"]
-       (for [snail-id (:endpoints group)]
-         [:div.result-list__item
-          [:a {:href (str "#/snail/" (name snail-id))}
-           [:img {:src (str "images/endpoint/" (get-in snails [snail-id :image]))
-                     :alt (str (get-in snails [snail-id :name]))}]
-          [:span (str (get-in snails [snail-id :name]))]]])])))
+    (html [:div
+           [:a {:href "#/"} "back button"]
+           [:h3 "Your snail could be one of these:"]
+           (for [snail-id (:endpoints group)]
+             [:div.result-list__item
+              [:a {:href (str "#/snail/" (name snail-id))}
+               [:img {:src (str "images/endpoint/" (get-in snails [snail-id :image]))
+                      :alt (str (get-in snails [snail-id :name]))}]
+               [:span (str (get-in snails [snail-id :name]))]]])])))
 
 
 (defn group-view
   [cursor _]
   (om/component
-    (html
-      [:div.snails-key
-       (let [group (snail-key-flat (get-in cursor [:current :question]))]
-         (om/build (partial group-list-component group) cursor))])))
+    (html [:div.snails-key
+           (let [group (snail-key-flat (get-in cursor [:current :question]))]
+             (om/build (partial group-list-component group) cursor))])))
